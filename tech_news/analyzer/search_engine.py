92c4eba58692f8a3ef75aa25e716741d35915c4d
Brayan Santos <brayan.santosrosa@gmail.com>
2023-03-14 14:42:54 -0300

    Requisito 07 concluido

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,13 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    response_db = search_news({'title': {'$regex': title, '$options': 'i'}})
+    list_tupas = []
+    for tup in response_db:
+        list_tupas.append((tup['title'], tup['url']))
+    return list_tupas
 
 
 # Requisito 8
@@ -11,3 +18,6 @@ def search_by_date(date):
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+
+print(search_by_title('UX'))
